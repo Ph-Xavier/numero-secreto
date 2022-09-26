8d590239b06c90b59985e66da59132d063047f8d
Pedro Xavier <pedroxxavier@hotmail.com>
2022-09-26 14:59:50 -0300

    Criando as dicas.

--- app/validacao.js
+++ app/validacao.js
@@ -14,6 +14,14 @@ function verificaSeOChutePossuiUmValorValido(chute) {
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
         `
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `
+    }else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        `
     }
 }
 
